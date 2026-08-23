cc8a5f33974a59a9e47dc2976910d6098b6cb520
LudwinReyes <LudwinReyes@users.noreply.github.com>
2026-08-22 23:29:13 -0500

    feat(analytics): integrar Google Tag Manager (GTM-M3JVGCV6)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { Metadata } from 'next';
 import { headers } from 'next/headers';
+import Script from 'next/script';
 import './globals.css';
 import { SiteConfigProvider } from '../contexts/SiteConfigContext';
 import { QuoteProvider } from '../contexts/QuoteContext';
@@ -71,12 +72,31 @@ export default async function RootLayout({
   return (
     <html lang="es">
       <head>
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M3JVGCV6');`,
+          }}
+        />
         <link rel="icon" href="/media/favicon.png" type="image/png" />
         <link rel="shortcut icon" href="/media/favicon.png" type="image/png" />
         <link rel="apple-touch-icon" href="/media/favicon.png" type="image/png" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
       <body className="min-h-screen bg-white font-sans text-gray-900">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M3JVGCV6"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <SiteConfigProvider>
           <QuoteProvider>
             <ScrollToTop />
